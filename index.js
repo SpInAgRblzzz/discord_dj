0be5b614e03d6dcd7b3c8ceab2bf84f54aa74d12
Kirill Kruchenok <spinagrblzzz@gmail.com>
2020-04-04 20:42:53 +0300

    feat: initial commands implemented

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const Discord = require("discord.js");
+const { Client, MessageEmbed } = require("discord.js");
 const { config } = require("dotenv");
 
-const client = new Discord.Client({
-	disableEveryone: true
+const client = new Client({
+	disableEveryone: true,
 });
 
 config({
-	path: __dirname + "/.env"
+	path: __dirname + "/.env",
 });
 
 client.on("ready", () => {
@@ -16,9 +16,32 @@ client.on("ready", () => {
 		status: "online",
 		game: {
 			name: "Getting developed",
-			type: "IMPROVING"
-		}
+			type: "IMPROVING",
+		},
 	});
+});
+
+client.on("message", async (message) => {
+	const prefix = "!";
+
+	if (message.author.bot) return;
+	if (!message.guild) return;
+	if (!message.content.startsWith(prefix)) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const comand = args.shift().toLowerCase();
+
+	//custom transmission
+	if (comand === "go") {
+		const connection = await message.member.voice.channel.join();
+		//const dispatcher = connection.play("/sample.mp3");
+		connection.play("sample.mp3", { volume: 1 });
+	}
+
+	if (comand === "leave") {
+		await message.member.voice.channel.leave();
+	}
+	//////
 });
 
 client.login(process.env.TOKEN);
