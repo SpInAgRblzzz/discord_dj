88b5d3d9986bb0f8fb9c51e6d149898ae84714fe
Kirill Kruchenok <spinagrblzzz@gmail.com>
2020-04-05 18:22:12 +0300

    feat: mistype commands added

--- src/commands.js
+++ src/commands.js
@@ -1,5 +1,5 @@
-exports.playCommands = ["play", "go", "start"];
-exports.leaveCommands = ["leave", "gtfo", "goaway"];
+exports.playCommands = ["play", "здфн", "go", "пщ", "start", "ыефке"];
+exports.leaveCommands = ["leave", "дуфму", "gtfo", "пеащ", "goaway", "пщфцфн"];
 exports.getCommandValidator = function getCommandValidator(command) {
 	return function (item) {
 		return item === command;
